4859fc71d8040cf3f79a8ab2e187416c538c09db
GamsayiniSomasekaram <gamsagamsi@gmail.com>
2025-06-16 22:11:59 +0530

    my latest

--- Calculater/src/Calculater01.jsx
+++ Calculater/src/Calculater01.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Calculater01() {
   return (
     <div>
-      
+      CALCULATER 
+      <h1>my simple Calculater</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit optio aliquid saepe aperiam corporis nobis cumque dolore sunt et quibusdam eius consequuntur, nesciunt alias fugit ipsam officia quasi distinctio recusandae?</p>
     </div>
   )
 }
