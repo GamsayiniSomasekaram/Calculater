4ce9f5d517c6d07cc7abc7010f52932101203180
GamsayiniSomasekaram <gamsagamsi@gmail.com>
2025-06-16 20:47:58 +0530

    my Calculater

---
+++ Calculater/src/Calculater01.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Calculater01() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Calculater01
---
+++ Calculater/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Calculater01 from './Calculater01'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+<Calculater01 />
+  </StrictMode>,
+)
