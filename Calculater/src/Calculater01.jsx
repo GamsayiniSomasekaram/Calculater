cf37ed6ea5d4f246543213d016bdc5419dd19487
GamsayiniSomasekaram <gamsagamsi@gmail.com>
2025-06-17 21:25:25 +0530

    my latest

--- Calculater/src/Calculater01.jsx
+++ Calculater/src/Calculater01.jsx
@@ -2,11 +2,41 @@ import React from 'react'
 
 function Calculater01() {
   return (
-    <div>
-      CALCULATER 
-      <h1>my simple Calculater</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit optio aliquid saepe aperiam corporis nobis cumque dolore sunt et quibusdam eius consequuntur, nesciunt alias fugit ipsam officia quasi distinctio recusandae?</p>
-    </div>
+  <>
+       <div className='w-full h-screen bg-red-900 flex justify-center items-center'>
+        <div className='  bg-[#171314] inset-shadow-sm inset-shadow-zinc-500'>
+          <div className='w-full p-2 bg-[#171314] inset-shadow-sm inset-shadow-zinc-500'></div>
+          <div className='grid p-4 grid-cols-4 gap-6'>
+            <div className='bg-[#171314] p-6 inset-shadow-sm rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1'></div>
+            <div className='bg-[#171314] p-6  inset-shadow-sm  rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1'></div>
+            <div className='bg-[#171314] p-6  inset-shadow-sm rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1'></div>
+            <div className='bg-[#171314] p-6 inset-shadow-sm rounded-2xl inset-shadow-zinc-500 hover:inset-shadow-red-500 hover:-translate-1'></div>
+
+
+            <div className='bg-[#bf0000] p-6 inset-shadow-sm rounded-2xl inset-shadow-zinc-50 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6  inset-shadow-sm rounded-2xl inset-shadow-zinc-50 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6  inset-shadow-sm rounded-2xl inset-shadow-zinc-50 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6  inset-shadow-sm rounded-2xl inset-shadow-zinc-50 hover:inset-shadow-red-50 hover:-translate-1'></div>
+            
+            <div className='bg-[#bf0000] p-6  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6 inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+
+            <div className='bg-[#bf0000] p-6  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+<div className='bg-[#bf0000] p-6 row-span-2 inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+
+            <div className='bg-[#bf0000] col-span-2   p-6 inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+            <div className='bg-[#bf0000] p-6  inset-shadow-lg rounded-2xl inset-shadow-zinc-100 hover:inset-shadow-red-50 hover:-translate-1'></div>
+            
+          </div>
+          </div>
+          
+       </div>
+    </>
+
   )
 }
 
